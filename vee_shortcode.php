4b59b0a8d9d32e6d38df9116f8aa174e7adea64f
Veesibility <Veesibility-test@users.noreply.github.com>
2017-01-03 00:42:34 +0100

    Update vee_shortcode.php

--- vee_shortcode.php
+++ vee_shortcode.php
@@ -1,4 +1,13 @@
 <?php
+/**
+* Plugin Name:      Custom Shortcode
+* Version:          1
+* Description:      Custom Shortcode
+* Author:           Veesibility
+* GitHub Plugin URI: https://github.com/Veesibility-test/shortcode
+      
+      
+         */
 /************* SHORTCODE *********************/
 
 // Add a current Year
